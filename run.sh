863eb40e2fcb0c14f2fdc8a003dd1d5042587413
ChangJay0212 <lastlife0212@gmail.com>
2025-02-03 16:20:32 +0800

    feat : save openwebui log

--- run.sh
+++ run.sh
@@ -10,7 +10,8 @@ source "${ROOT}/setting/utils/tools.sh"
 NGINX_CONF="${ROOT}/setting/nginx.conf"
 CONF="${ROOT}/setting/setting.json"
 COMPOSE="${ROOT}/docker-compose.yml"
-
+OPEN_WEB_UI_LOG_DIR="./open_web_ui"
+OPEN_WEB_UI_LOG_FILE="openwebui.log"
 # Check configuration is exit
 check_config ${CONF}
 check_jq
@@ -27,4 +28,10 @@ update_docker_compose_port "$COMPOSE" "nginx" "$NGINX_PORT"
 update_docker_compose_port "$COMPOSE" "open_webui" "$OPEN_WEB_UI_PORT"
 
 
-docker compose -f ${COMPOSE} up
+#Use '-d' move service to background and than we can run the next code.
+docker compose -f ${COMPOSE} up -d 
+
+sleep 5
+
+mkdir -p "$OPEN_WEB_UI_LOG_DIR"
+docker logs -f open_webui 2>&1 | split -b 5M -d --filter='sh -c "cat > '${OPEN_WEB_UI_LOG_DIR}'/'${OPEN_WEB_UI_LOG_FILE}'"'
