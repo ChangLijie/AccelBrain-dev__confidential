eefe355e616d3e00b9951d34a21b588de371b88c
ChangLijie <122772998+ChangLijie@users.noreply.github.com>
2025-02-04 16:37:07 +0800

    feat : background execution

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 # Store the utilities
 FILE=$(realpath "$0")
 ROOT=$(dirname "${FILE}")
@@ -12,7 +11,9 @@ CONF="${ROOT}/setting/setting.json"
 COMPOSE="${ROOT}/docker-compose.yml"
 OPEN_WEB_UI_LOG_DIR="./openwebui/log"
 OPEN_WEB_UI_LOG_FILE="openwebui.log"
-# Check configuration is exit
+WAITING_TIME=5
+
+# Check configuration is exist
 check_config ${CONF}
 check_jq
 
@@ -23,15 +24,21 @@ OLLAMA_MAX_LOADED_MODELS=$(cat "${CONF}" | jq -r '.ollama.OLLAMA_MAX_LOADED_MODE
 MODEL_HANDLER_PORT=$(cat "${CONF}" | jq -r '.model_handler.port')
 
 update_compose_env ${COMPOSE} "OLLAMA_MAX_LOADED_MODELS=${OLLAMA_MAX_LOADED_MODELS}"
-
 update_docker_compose_port "$COMPOSE" "nginx" "$NGINX_PORT"
 update_docker_compose_port "$COMPOSE" "open_webui" "$OPEN_WEB_UI_PORT"
 
+# Start Docker Compose in the background
+docker compose -f ${COMPOSE} up -d --force-recreate
 
-#Use '-d' move service to background and than we can run the next code.
-docker compose -f ${COMPOSE} up -d 
+# Countdown sleep with progress bar
 
-sleep 5
+while [ $WAITING_TIME -gt 0 ]; do
+    echo -ne "Waiting to start Accelbrain... $WAITING_TIME s\r"
+    sleep 1
+    ((WAITING_TIME--))
+done
+echo -e "\nAccelbrain started!"
 
 mkdir -p "$OPEN_WEB_UI_LOG_DIR"
-docker logs -f open_webui 2>&1 | split -b 5M -d --filter='sh -c "cat > '${OPEN_WEB_UI_LOG_DIR}'/'${OPEN_WEB_UI_LOG_FILE}'"'
+
+nohup bash -c "docker logs -f open_webui 2>&1 | split -b 5M -d --filter='sh -c \"cat > ${OPEN_WEB_UI_LOG_DIR}/${OPEN_WEB_UI_LOG_FILE}\"'" > /dev/null 2>&1 &
